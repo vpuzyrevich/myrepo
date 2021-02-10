f8f699529e081a7c725fd6898cd3c5a316c9fd95
viktoria <v.puzyrevich.com>
2021-02-10 22:01:46 +0300

    add script

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,27 @@
 'use strict';
 
-let numberOfFilms = 5;
+let numberOfFilms = +prompt('Сколько фильмов вы смотрели?','');
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: "",
-    actors: "",
+    movies: {},
+    actors: {},
     genres: [],
     privat: false
 };
+
+const a = prompt('Один из последний просмотренных фильмов?',''),
+b = prompt('На сколько оцените его?',''),
+c = prompt('Один из последний просмотренных фильмов?',''),
+d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+
+
+
